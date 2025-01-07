9e09c8c2ab1d4039df3347293668f165ffe58b9b
Benni <miuta.beni@gmail.com>
2025-01-07 11:08:37 +0100

    Create WordSuffixChecker.java
    Create a problem that checks if the first word is a suffix of the other one, and count how many follow the rule

---
+++ Strings of characters/WordSuffixChecker.java
@@ -0,0 +1,20 @@
+package Strings of characters;
+
+public class WordSuffixChecker {
+    public static void main(String[] args) {
+        int n = 6;
+        String[] words = {"ion", "revelion", "ionel", "bulion", "milion", "pionul"}; 
+        
+        String firstWord = words[0];
+        int count = 0;
+        
+        for (int i = 1; i < n; i++) {
+            if (words[i].endsWith(firstWord)) {
+                count++;
+            }
+        }
+        
+        System.out.println(count);
+    }
+}
+
