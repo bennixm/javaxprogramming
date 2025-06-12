90e48a35560599d95c480b04ddf19aa9b1376004
Benni <miuta.beni@gmail.com>
2025-06-12 15:23:41 +0200

    dsadsa
    dsadsa

---
+++ APIS/pm2miniproject/Class17Feb/test.java
@@ -0,0 +1,15 @@
+package APIS.pm2miniproject.Class17Feb;
+
+public class test {
+        public static void main(String[] args) {
+            System.out.println(mystery(5.0));
+        }
+    
+        public static int mystery(int x) {
+            return x * 2;
+        }
+    
+        public static double mystery(double x) {
+            return x / 2;
+        }
+}
